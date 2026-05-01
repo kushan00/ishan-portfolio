b182187b466f932422f89ad3ec5191e07168f9ff
kushan00 <kushandimantha533@gmail.com>
2026-05-01 08:34:49 +0530

    image updates

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.GITHUB_PAGES === "true";
+const isProd = process.env.NODE_ENV === "production";
 const basePath = isProd ? "/ishan-portfolio" : "";
 
 const nextConfig: NextConfig = {
